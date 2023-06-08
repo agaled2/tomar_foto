aefc00c82c27add0a0ced1e3a916abc198a6c907
agaled2 <130106607+agaled2@users.noreply.github.com>
2023-06-08 15:17:50 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+var reconocimientoDeVos = window.webkitSpeechRecognition;
+var reconocimiento = new reconocimientoDeVos();
+reconocimiento.lang = "es-MX";
+
+function iniciar(){
+  reconocimiento.start();
+}
+
+reconocimiento.onresult = function (resultado){
+  console.log(resultado)
+  textoDetectado = resultado.results[0][0].transcript;
+  document.getElementById("textbox").value = textoDetectado;
+  hablar(textoDetectado)
+  if(textoDetectado.toLowerCase() == "hola"){
+    Webcam.attach("#camara")
+    hablar("cinco  cuatro tres  dos  uno")
+    setTimeout(tomarFoto, 5000)
+  }
+}
+
+function hablar(mensaje){
+
+
+leeEnVozAlta = window.speechSynthesis;
+  var lectura  = new SpeechSynthesisUtterance(mensaje)
+  lectura.lang = "es-MX"
+  leeEnVozAlta.speak(lectura);
+}
+
+Webcam.set({
+  width:360,
+  height:250,
+  image_format:"png",
+  png_quality:90
+  
+})
+
+function tomarFoto(){
+  Webcam.snap(function(foto){
+  document.getElementById("resultado").innerHTML = '<img src="' + foto + '" id="fotos">'
+    guardar();
+})
+}
+
+function guardar(){
+  descarga = document.getElementById("link");
+    descarga.href = document.getElementById("fotos").src
+      descarga.click();
